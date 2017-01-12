abf05108c384de88b906fbae6560ba0b93e3bbc2
Brian Chan <brian.chan080@gmail.com>
2017-01-12 21:27:58 +0000

    tiny turtle

---
+++ script.js
@@ -0,0 +1,26 @@
+/* global TinyTurtle */ 
+
+TinyTurtle.apply(window); 
+forward (35);
+left (90);
+forward (70);
+left (90); 
+forward (35);
+left (90); 
+forward (70);
+stamp ();
+
+forward (60);
+right (120);
+forward (60);
+right (120); 
+forward (60);
+stamp ();
+
+right (30); 
+forward (35);
+left (60);
+forward (40);
+left (60);
+forward (40);
+stamp ();
