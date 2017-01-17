f64dde57e61e8af0bb23a20005e20930b043943b
Brian Chan <brian.chan080@gmail.com>
2017-01-17 22:07:38 +0000

    created function for various shapes

--- script.js
+++ script.js
@@ -3,9 +3,11 @@
 /* global left */
 /* global right */ 
 /* global stamp */ 
+/* global penUp */
 
 
 TinyTurtle.apply(window); 
+/*
 forward (35);
 left (90);
 forward (70);
@@ -21,7 +23,7 @@ forward (60);
 right (120); 
 forward (60);
 stamp ();
-
+ 
 right (30); 
 forward (35);
 left (60);
@@ -29,3 +31,34 @@ forward (40);
 left (60);
 forward (40);
 stamp ();
+ */
+/*
+penUp;  
+right (30); 
+
+
+right(60);
+forward (30);
+left (60); 
+forward (30);
+left (60);
+forward (30);
+left (60);
+forward (30);
+left (60);
+forward (30);
+left (60);
+forward (30);
+right (120);
+forward (20);
+stamp ();
+*/
+/*i represents the sides present */
+function box(length) {
+    for (var i = 0; i < 4 ; i++ )
+    {
+        forward(length); 
+    right(90); 
+    }
+}
+box(70)
